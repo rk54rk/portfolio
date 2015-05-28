97981f5b86d6204652b7582b3b25732754e1b890
Rongkai <rk54rk@hotmail.com>
2015-05-29 00:01:09 +0100

    UI building, animation etc.
    Signed-off-by: Rongkai <rk54rk@hotmail.com>

--- app/app.js
+++ app/app.js
@@ -1,7 +1,2 @@
 var app = angular.module("app", []);
-
-app.filter('reverse', function() {
-  return function(items) {
-    return items.slice().reverse();
-  };
-});
+var home_url = document.location.origin;
--- scripts.js
+++ scripts.js
@@ -1,21 +1,9 @@
-var c = $('.b')[0];
-c.onclick = function() {
- // Trigger the `hover` event on the paragraph
- c.onhover.call(p);
-};
-
-div_porportion();
-
-function div_porportion(){
-	var b1 = $('.b1').width();
-	$('.b1').css({'height':b1+'px'});
-	
-	var b2 = $('.b2').width();
-	$('.b2').css({'height':b2/2+'px'});
-	
-	var b3 = $('.b3').width();
-	$('.b3').css({'height':b3*2+'px'});
-	
-	var b4 = $('.b4').width();
-	$('.b4').css({'height':b4+'px'});
-}
+$('#modal-image').on('show.bs.modal', function (event) {
+  var button = $(event.relatedTarget); // Button that triggered the modal
+  var src_image = button.data('image'); // Extract info from data-* attributes
+  // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+  // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+  var modal = $(this);
+  modal.find('.big-img').css( "background-image", 'url(' + src_image + ')');
+  
+})
