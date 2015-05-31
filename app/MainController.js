c85f4a3140aff18f43deab40199c3e91544709dc
Rongkai <rk54rk@hotmail.com>
2015-05-31 20:23:53 +0100

    making gallery

--- app/MainController.js
+++ app/MainController.js
@@ -1,4 +1,4 @@
-app.controller('MainController', ['$scope', '$http', function($scope, $http) { 
+app.controller('MainController', ['$scope', '$http', '$timeout', function($scope, $http, $timeout) { 
   $http.get('../content/index.json').success(function(data) {
     $scope.data = data;
     document.title = data.title;
@@ -13,7 +13,21 @@ $('#modal-image').on('show.bs.modal', function (event) {
   // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
   // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
   src_image="content/projects/" + project + "/" + project + "_" + imageid + "_l.jpg";
-  console.log(src_image);
-  $(this).find('.big-img').css( "background-image", 'url(' + src_image + ')');
-  
-})
+  $(this).find('.bigimg').css( "background-image", 'url(' + src_image + ')');
+});
+
+
+//define "project" directive, set height of .caption div to equal the row height
+app.directive("project", function($timeout) {
+  return {
+    restrict: "A",
+    link: function(scope, elem, attrs) {
+      $timeout(function () {
+        var rowHeight = elem[0].offsetHeight;
+        console.log(rowHeight);
+        //$(elem).find('.caption').css('height', rowHeight);
+        scope.height = rowHeight + "px";
+      });
+    }
+  }
+});
