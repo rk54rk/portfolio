e9b5c4eba7664916446d8b93c62c87cda4516475
Rongkai <rk54rk@hotmail.com>
2015-06-01 12:37:42 +0100

    finished gallery

--- app/MainController.js
+++ app/MainController.js
@@ -13,23 +13,41 @@ $('#modal-image').on('show.bs.modal', function (event) {
 
 //changing image when clicked prev/next buttons
 function changeImg(from_elem){
+  
   var elem = $(from_elem);
   var project = elem.data('project');
   var imageid = elem.data('imageid');
-  src_image="content/projects/" + project + "/" + project + "_" + imageid + "_l.jpg";
-  $(document).find('.bigimg').css( "background-image", 'url(' + src_image + ')');
+  var imagecount = elem.data('imagecount');
   
-  console.log('from elem:'+from_elem);
-  console.log('changedto:'+project+", "+imageid);
+  var current_image="content/projects/" + project + "/" + project + "_" + imageid + "_l.jpg";
+  $(document).find('#bigimg').css( "background-image", 'url(' + current_image + ')');
+  
+  if ((imageid + 1) <= imagecount){
+    var next_image = "content/projects/" + project + "/" + project + "_" + (imageid + 1) + "_l.jpg";
+    $(document).find('#bigimg-next').css( "background-image", 'url(' + next_image + ')');
+  }
+  if ((imageid - 1) > 0){
+    var prev_image = "content/projects/" + project + "/" + project + "_" + (imageid - 1) + "_l.jpg";
+    $(document).find('#bigimg-prev').css( "background-image", 'url(' + prev_image + ')');
+  }
   
   $(document).find('#bigimg-select-prev').data('project', project);
   $(document).find('#bigimg-select-next').data('project', project);
   $(document).find('#bigimg-select-prev').data('imageid', imageid - 1);
   $(document).find('#bigimg-select-next').data('imageid', imageid + 1);
+  $(document).find('#bigimg-select-prev').data('imagecount', imagecount);
+  $(document).find('#bigimg-select-next').data('imagecount', imagecount);
   
-  project = null;
-  imageid = null;
-  elem = null;
+  if ($(document).find('#bigimg-select-prev').data('imageid') < 1){
+    $("#bigimg-select-prev").css("display", "none");
+  } else {
+    $("#bigimg-select-prev").css("display", "block");
+  }
+  if ($(document).find('#bigimg-select-next').data('imageid') > imagecount){
+    $("#bigimg-select-next").css("display", "none");
+  } else {
+    $("#bigimg-select-next").css("display", "block");
+  }
 }
 
 //define "project" directive, set height of .caption div to equal the row height
