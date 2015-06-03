9c47b46041ad953a4481b4198d6bb11db504f9ed
Rongkai <rk54rk@hotmail.com>
2015-06-03 15:14:52 +0100

    typography, now supposrt html content

--- app/MainController.js
+++ app/MainController.js
@@ -1,8 +1,14 @@
-app.controller('MainController', ['$scope', '$http', '$timeout', function($scope, $http, $timeout) { 
+app.controller('MainController', ['$scope', '$http', '$timeout', '$sce', function($scope, $http, $timeout, $sce) { 
   $http.get('../content/index.json').success(function(data) {
     $scope.data = data;
     $scope.current = "";
     document.title = data.title;
+    
+    $scope.data.about.contact = $sce.trustAsHtml(data.about.contact);
+    $scope.data.about.desc = $sce.trustAsHtml(data.about.desc);
+    $scope.data.projects.forEach(function(project) {
+      project.desc = $sce.trustAsHtml(project.desc);
+    });
   });
 }]);
 
